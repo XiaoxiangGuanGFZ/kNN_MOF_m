4bbfae65242b4ff5a4a2d1a68cfbd352bd0e07ed
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-05-29 09:57:31 +0200

    update

--- scr/main.c
+++ scr/main.c
@@ -6,6 +6,8 @@
 #include "def_struct.h"
 #include "Func_dataIO.h"
 #include "Func_Initialize.h"
+#include "Func_Prepro.h"
+#include "Func_Print.h"
 #include "Func_SSIM.h"
 #include "Func_Disaggregate.h"
 // #include "Func_Covariate.h"
@@ -29,6 +31,7 @@
 
 FILE *p_SSIM;
 FILE *p_log;  // file pointer pointing to log file
+int f_prep;   // flag for preprocessing the data with normalization or standardization
 
 /*****************
  * main function
@@ -59,68 +62,15 @@ int main(int argc, char * argv[]) {
         printf("cannot create / open log file\n");
         exit(1);
     }
-    printf("------ Global parameter import completed: %s", ctime(&tm));
-    fprintf(p_log, "------ Global parameter import completed: %s", ctime(&tm));
-
-    printf(
-        "FP_DAILY: %s\nFP_HOULY: %s\nFP_OUT:   %s\nFP_LOG:   %s\n",
-        p_gp->FP_DAILY, p_gp->FP_HOURLY, p_gp->FP_OUT, p_gp->FP_LOG
-    );
-    fprintf(p_log, "FP_DAILY: %s\nFP_HOULY: %s\nFP_OUT:   %s\nFP_LOG:   %s\n",
-        p_gp->FP_DAILY, p_gp->FP_HOURLY, p_gp->FP_OUT, p_gp->FP_LOG);
-
-    // if (p_gp->VAR == 5)
-    // {
-    //     printf(
-    //         "FP_COV_DLY: %s\nFP_COV_HLY: %s\n",
-    //         p_gp->FP_COV_DLY, p_gp->FP_COV_HLY);
-    //     fprintf(p_log, "FP_COV_DLY: %s\nFP_COV_HLY: %s\n",
-    //             p_gp->FP_COV_DLY, p_gp->FP_COV_HLY);
-    // }
-
-    printf(
-        "------ Disaggregation parameters: -----\nVAR: %s\nMONTH: %s\nN_STATION: %d\nCONTINUITY: %d\nSEASON: %s\n",
-        VARname, p_gp->MONTH, p_gp->N_STATION, p_gp->CONTINUITY, p_gp->SEASON
-    );
-    fprintf(
-        p_log,
-        "------ Disaggregation parameters: -----\nVAR: %s\nMONTH: %s\nN_STATION: %d\nCONTINUITY: %d\nSEASON: %s\n",
-        VARname, p_gp->MONTH, p_gp->N_STATION, p_gp->CONTINUITY, p_gp->SEASON
-    );
-    if (strncmp(p_gp->SEASON, "TRUE", 4) == 0)
-    {
-        printf("SUMMER: %d-%d\n", p_gp->SUMMER_FROM, p_gp->SUMMER_TO);
-        fprintf(p_log,"SUMMER: %d-%d\n", p_gp->SUMMER_FROM, p_gp->SUMMER_TO);
-    }
-    printf("SSIM_K: %f,%f,%f\nSSIM_power: %f,%f,%f\nNODATA: %f\n",
-           p_gp->k[0], p_gp->k[1], p_gp->k[2], p_gp->power[0], p_gp->power[1], p_gp->power[2],
-           p_gp->NODATA);
-    fprintf(p_log, "SSIM_K: %f,%f,%f\nSSIM_power: %f,%f,%f\nNODATA: %f\n",
-            p_gp->k[0], p_gp->k[1], p_gp->k[2], p_gp->power[0], p_gp->power[1], p_gp->power[2],
-            p_gp->NODATA);
-
+    Print_gp(p_gp);
+    f_prep = p_gp->PREPROCESS;
     /******* import circulation pattern series *********/
     
     static struct df_cp df_cps[MAXrow];
     int nrow_cp=0;  // the number of CP data columns: 4 (y, m, d, cp)
     if (strncmp(p_gp->T_CP, "TRUE", 4) == 0) {
         nrow_cp = import_df_cp(Para_df.FP_CP, df_cps);
-        time(&tm);
-        printf("------ Import CP data series (Done): %s", ctime(&tm)); 
-        fprintf(p_log, "------ Import CP data series (Done): %s", ctime(&tm));
-
-        printf("* number of CP data rows: %d\n", nrow_cp); 
-        fprintf(p_log, "* number of CP data rows: %d\n", nrow_cp);
-
-        printf("* the first day: %d-%02d-%02d \n", df_cps[0].date.y, df_cps[0].date.m, df_cps[0].date.d);
-        fprintf(p_log, "* the first day: %d-%02d-%02d \n", df_cps[0].date.y, df_cps[0].date.m, df_cps[0].date.d);
-        
-        printf("* the last day:  %d-%02d-%02d \n", 
-            df_cps[nrow_cp-1].date.y, df_cps[nrow_cp-1].date.m, df_cps[nrow_cp-1].date.d
-        );
-        fprintf(p_log, "* the last day:  %d-%02d-%02d \n", 
-            df_cps[nrow_cp-1].date.y, df_cps[nrow_cp-1].date.m, df_cps[nrow_cp-1].date.d
-        );
+        Print_cp(df_cps, nrow_cp);
     } 
     if (strncmp(p_gp->MONTH, "TRUE", 4) == 0)
     {
@@ -136,63 +86,29 @@ int main(int argc, char * argv[]) {
     }
     
     /****** import daily rainfall data (to be disaggregated) *******/
-    
     static struct df_rr_d df_dly[MAXrow];
     int nrow_rr_d;
     nrow_rr_d = import_dfrr_d(Para_df.FP_DAILY, Para_df.N_STATION, df_dly);
     initialize_dfrr_d(p_gp, df_dly, df_cps, nrow_rr_d, nrow_cp);
-    Normalize_d(p_gp, df_dly, nrow_rr_d);
-
-    time(&tm);
-    printf("------ Import daily data (Done): %s", ctime(&tm)); fprintf(p_log, "------ Import daily data (Done): %s", ctime(&tm));
-    
-    printf("* the total rows: %d\n", nrow_rr_d); fprintf(p_log, "* the total rows: %d\n", nrow_rr_d);
-    
-    printf("* the first day: %d-%02d-%02d\n", df_dly[0].date.y,df_dly[0].date.m,df_dly[0].date.d);
-    fprintf(p_log, "* the first day: %d-%02d-%02d\n", df_dly[0].date.y,df_dly[0].date.m,df_dly[0].date.d);
-
-    printf(
-        "* the last day:  %d-%02d-%02d\n", 
-        df_dly[nrow_rr_d-1].date.y,df_dly[nrow_rr_d-1].date.m,df_dly[nrow_rr_d-1].date.d
-    );
-    fprintf(
-        p_log,
-        "* the last day:  %d-%02d-%02d\n", 
-        df_dly[nrow_rr_d-1].date.y,df_dly[nrow_rr_d-1].date.m,df_dly[nrow_rr_d-1].date.d
-    );
-    printf("* the total classes:  %d\n", p_gp->CLASS_N);
-    fprintf(p_log, "* the total classes:  %d\n", p_gp->CLASS_N);
-    view_class_rrd(df_dly, nrow_rr_d);
+    Print_dly(df_dly, p_gp, nrow_rr_d);
 
     /****** import hourly rainfall data (obs as fragments) *******/
-    
     int ndays_h;
     static struct df_rr_h df_hly[MAXrow];
     ndays_h = import_dfrr_h(p_gp->VAR, Para_df.FP_HOURLY, Para_df.N_STATION, df_hly);
-    Normalize_h(p_gp, df_hly, ndays_h);
-
     initialize_dfrr_h(p_gp, df_hly, df_cps, ndays_h, nrow_cp);
-    time(&tm);
-    printf("------ Import hourly data (Done): %s", ctime(&tm)); fprintf(p_log, "------ Import hourly data (Done): %s", ctime(&tm));
-    
-    printf("* total hourly obs days: %d\n", ndays_h); fprintf(p_log, "* total hourly obs days: %d\n", ndays_h);
-    
-    printf("* the first day: %d-%02d-%02d\n", df_hly[0].date.y, df_hly[0].date.m, df_hly[0].date.d);
-    fprintf(p_log, "* the first day: %d-%02d-%02d\n", df_hly[0].date.y, df_hly[0].date.m, df_hly[0].date.d);
-    
-    printf(
-        "* the last day:  %d-%02d-%02d\n", 
-        df_hly[ndays_h-1].date.y, df_hly[ndays_h-1].date.m, df_hly[ndays_h-1].date.d
-    );
-    fprintf(
-        p_log,
-        "* the last day:  %d-%02d-%02d\n", 
-        df_hly[ndays_h-1].date.y, df_hly[ndays_h-1].date.m, df_hly[ndays_h-1].date.d
-    );
-    view_class_rrh(df_hly, ndays_h);
-    /****** maxima of rainfall value (L value in SSIM algorithm) *******/
-    // initialize_L(df_hly, df_dly, p_gp, nrow_rr_d, ndays_h);
+    Print_hly(df_hly, ndays_h);
+    /****** preprocessing *******/
+    if (f_prep == 1)
+    {
+        Normalize(p_gp, df_dly, df_hly, nrow_rr_d, ndays_h);
+    }
+    else if (f_prep == 2)
+    {
+        Standardize(p_gp, df_dly, df_hly, nrow_rr_d, ndays_h);
+    }
 
+    /****** covariate *******/
     // static struct df_rr_d df_dly_cov[MAXrow];
     // static struct df_rr_h df_hly_cov[MAXrow];
     // if (p_gp->VAR == 5)
